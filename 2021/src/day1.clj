4570bb5e866a5ed777b869cdc04df5402b90e914
Vincenzo Chianese <vincenz.chianese@icloud.com>
2021-12-01 09:57:22 -0600

    format

--- 2021/src/day1.clj
+++ 2021/src/day1.clj
@@ -3,15 +3,17 @@
 
 (def input (into [] (map #(. Integer parseInt %) (split-lines (clojure.core/slurp "./input/day1input.txt")))))
 
-(def part1 (reduce
-            (fn [{:keys [count cur]} val]
-              (let [nextCount (if (> val cur) (inc count) count)]
-                {:cur val :count nextCount}))
-            {:count 0 :cur Integer/MAX_VALUE} input))
+(def part1
+  (reduce
+   (fn [{:keys [count cur]} val]
+     (let [nextCount (if (> val cur) (inc count) count)]
+       {:cur val :count nextCount}))
+   {:count 0 :cur Integer/MAX_VALUE} input))
 
-(def part2 (reduce-kv
-            (fn [{:keys [count cur]} index _]
-              (let [current (apply + (subvec (into input [0 0]) index (+ 3 index)))
-                    nextCount (if (> current cur) (inc count) count)]
-                {:cur current :count nextCount}))
-            {:count 0 :cur Integer/MAX_VALUE} input))
+(def part2
+  (reduce-kv
+   (fn [{:keys [count cur]} index _]
+     (let [current (apply + (subvec (into input [0 0]) index (+ 3 index)))
+           nextCount (if (> current cur) (inc count) count)]
+       {:cur current :count nextCount}))
+   {:count 0 :cur Integer/MAX_VALUE} input))
