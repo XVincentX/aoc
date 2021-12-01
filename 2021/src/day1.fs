c986f46ff89460d0ea12e6f35e392ce3c794da44
Vincenzo Chianese <vincenz.chianese@icloud.com>
2021-12-01 14:18:10 -0600

    fsharp

---
+++ 2021/src/day1.fs
@@ -0,0 +1,36 @@
+type foldState = { inc: int; cur: int }
+
+let input =
+    "./input/day1input.txt"
+    |> System.IO.File.ReadAllLines
+    |> Array.map (fun x -> System.Int32.Parse x)
+
+let p1 =
+    input
+    |> Array.fold
+        (fun acc value ->
+            if value > acc.cur then
+                { inc = acc.inc + 1; cur = value }
+            else
+                { inc = acc.inc; cur = value })
+        { inc = 0; cur = System.Int32.MaxValue }
+
+printfn "%i" p1.inc
+
+let p2 =
+    input
+    |> Array.indexed
+    |> Array.fold
+        (fun acc value ->
+            let index = fst value
+
+            let filled = Array.append input [| 0; 0 |]
+            let sum = filled.[index..index + 2]|> Array.sum
+
+            if sum > acc.cur then
+                { inc = acc.inc + 1; cur = sum }
+            else
+                { inc = acc.inc; cur = sum })
+        { inc = 0; cur = System.Int32.MaxValue }
+
+printfn "%i" p2.inc
