770ba7a96bd29a4df2cdb293fcf937560751e926
Vincenzo Chianese <vincenz.chianese@icloud.com>
2021-12-02 07:35:35 -0600

    thread macro is better

--- 2021/src/day2.clj
+++ 2021/src/day2.clj
@@ -15,19 +15,15 @@
          "down" (update position :depth + iValue)
          "up" (update position :depth - iValue))))
    {:horizontal 0 :depth 0} input))
-
-
-(defn upd [p val]
-  (-> p 
-  (update :horizontal + val)
-  (update :depth + (* val (:aim p)))))
  
 (def part2
   (reduce
    (fn [position [movement value]]
      (let [iValue (Integer/parseInt value)]
        (condp = movement
-         "forward" (upd position iValue)
+         "forward" (-> position
+                       (update :horizontal + iValue)
+                       (update :depth + (* iValue (:aim position))))
          "down" (update position :aim + iValue)
          "up" (update position :aim - iValue))))
    {:horizontal 0 :depth 0 :aim 0} input))
