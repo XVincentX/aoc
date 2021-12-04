94a1dedc0d283d426a578626e6f7943ebd93f86b
Vincenzo Chianese <vincenz.chianese@icloud.com>
2021-12-03 18:30:10 -0600

    day3 part 1

---
+++ 2021/src/day3.fs
@@ -0,0 +1,40 @@
+module day3
+
+open FSharpx.Text
+
+type foldState = { gamma: string; epsilon: string }
+
+let input =
+    "./input/day3input.txt"
+    |> System.IO.File.ReadAllLines
+    |> Array.map Strings.toCharArray
+
+let input_freqs =
+    input
+    |> Array.transpose
+    |> Array.map (fun x -> Array.countBy id x)
+
+let p1 =
+    Array.fold
+        (fun acc (value: (char * int) []) ->
+            let freq1 = value.[0]
+            let freq2 = value.[1]
+
+            let most_common =
+                if snd freq1 > snd freq2 then
+                    fst freq1
+                else
+                    fst freq2
+
+            let parsed =
+                System.Int32.Parse(most_common.ToString())
+
+            let reversed = parsed ^^^ 1
+
+            { gamma = acc.gamma + parsed.ToString()
+              epsilon = acc.epsilon + reversed.ToString() })
+
+        { gamma = "0"; epsilon = "0" }
+        input_freqs
+
+printfn "%A" p1
